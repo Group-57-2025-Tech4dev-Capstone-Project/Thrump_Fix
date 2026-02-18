53ccee72b61eea8008fda3947599588f53d76e7b
Ruqayyah <ruqayyahadebanjo22@gmail.com>
2026-02-18 16:00:13 +0100

    Update LandingPage

--- Frontend/src/pages/publicUsers/LandingPage.jsx
+++ Frontend/src/pages/publicUsers/LandingPage.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../Components/landing/Navbar";
 import Hero from "../../Components/landing/Hero"
+import Promise from "../../Components/landing/Promise"
 
 export default function LandingPage(){
     return(
         <section>
             <Navbar/>
             <Hero/>
+            <Promise/>
         </section>
     )
 }
