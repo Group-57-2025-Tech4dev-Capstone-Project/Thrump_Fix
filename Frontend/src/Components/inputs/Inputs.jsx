10594939a8e3f71215e7e05efc6fb07cfaaee650
Ruqayyah <ruqayyahadebanjo22@gmail.com>
2026-02-19 23:18:19 +0100

    Bring corrected shared components from signup

--- Frontend/src/Components/inputs/Inputs.jsx
+++ Frontend/src/Components/inputs/Inputs.jsx
@@ -1,9 +1,24 @@
+import "./Inputs.css"
+export default function Input({
+  label,
+  type = "text",
+  placeholder,
+  error,
+  ...rest
 
-function Inputs(){
-    return(
-        <section>
-            
-        </section>
-    )
-
+}) {
+  return (
+    <div className="input-field">
+      <label>{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        {...rest}
+        className={error ? "error" : ""}
+      />
+      {error && <p className="errorText">{error}</p>}
+    </div>
+  );
 }
+
+
