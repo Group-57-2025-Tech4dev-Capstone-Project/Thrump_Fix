6cdca77fcb2166f338e3170c3c008c9f399d4ff1
Favour Michael <chinazamfae@gmail.com>
2026-02-27 23:11:49 +0000

    Fix case-sensitive import path for Card component

--- Frontend/src/pages/publicUsers/PricingPage.jsx
+++ Frontend/src/pages/publicUsers/PricingPage.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { Card } from "../../components/Card/Card";
+import { Card } from "../../components/card/Card";
 import Button from "../../components/buttons/Buttons";
 import route from "../../utils/routes";
 
